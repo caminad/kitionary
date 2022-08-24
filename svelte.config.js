b2e67e59fc766140569e9e67d623ec78f58419fe
David Jones <david@omg.lol>
2022-08-24 17:14:31 +0100

    Deploy to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: '/kitionary',
+		},
+		prerender: {
+			default: true,
+		},
+		trailingSlash: 'always',
 	},
 };
 
