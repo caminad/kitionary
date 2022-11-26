4f5a10f8d99f206fa4f3ef52b3af478c9d166c13
caminad <418@omg.lol>
2022-11-26 14:58:48 +0000

    Use compiler for attribute interpolation (#174)

--- src/lib/WiktionaryLink.svelte
+++ src/lib/WiktionaryLink.svelte
@@ -5,5 +5,5 @@
 
 <a
 	class="text-blue-600 hover:text-blue-800 hover:underline hover:underline-offset-2"
-	href="https://en.wiktionary.org/wiki/{word}">{word.replace(/_/g, ' ')}</a
+	href="https://en.wiktionary.org/wiki/{word}">{word.replace(/_/g, " ")}</a
 >
